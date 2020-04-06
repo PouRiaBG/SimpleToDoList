671d8b01065ace64ce55099557e66cbbfb13a0e3
PouRiaBG <pooria.bonyadgozar@gmail.com>
2020-04-06 23:26:25 +0430

    Added Add and remove task feature

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.getElementsByTagName("body")[0].addEventListener("load", loading);
+//Add Today Date in the header
 
 function loading() {
     var date = new Date().toDateString();
@@ -10,4 +10,34 @@ function loading() {
     var element = document.getElementById("header-date");
 
     element.appendChild(textDate);
+}
+
+var addElement = document.getElementById("add");
+
+addElement.addEventListener("click", value);
+
+function value() {
+    var task = document.getElementsByTagName("input")[0];
+    if (task.value.length === 0) {
+        alert("Dude!! this is empty");
+    } else {
+        addTask(task);
+    }
+}
+
+function addTask(inputElement) {
+    var parentList = document.getElementById("list").childNodes[1];
+    var trashIcon = document.createElement("i");
+    var removeAttr = document.createAttribute("onclick");
+    removeAttr.value = "this.parentNode.remove()";
+    trashIcon.setAttributeNode(removeAttr);
+    trashIcon.setAttribute("class", "far fa-trash-alt");
+    //remove element 
+
+    var li = document.createElement("li");
+    var task = document.createTextNode(inputElement.value);
+    li.appendChild(task);
+    li.appendChild(trashIcon);
+    parentList.appendChild(li);
+    inputElement.value = "";
 }
--- src/index.js
+++ src/index.js
@@ -9,3 +9,35 @@ function loading(){
 
     element.appendChild(textDate)
 }
+
+let addElement = document.getElementById("add");
+
+addElement.addEventListener("click",value);
+
+
+function value(){
+    const task = document.getElementsByTagName("input")[0];
+    if(task.value.length === 0){
+        alert("Dude!! this is empty")
+    }else{
+        addTask(task);
+    }
+}
+
+function addTask(inputElement){
+    const parentList = document.getElementById("list").childNodes[1];
+    const trashIcon = document.createElement("i");
+    const removeAttr = document.createAttribute("onclick");
+    removeAttr.value = "this.parentNode.remove()"
+    trashIcon.setAttributeNode(removeAttr)
+    trashIcon.setAttribute("class","far fa-trash-alt")
+    //remove element 
+    
+    const li = document.createElement("li")
+    var task = document.createTextNode(inputElement.value)
+    li.appendChild(task)
+    li.appendChild(trashIcon)
+    parentList.appendChild(li)
+    inputElement.value = ""
+    
+}
